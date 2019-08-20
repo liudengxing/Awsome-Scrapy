964f62498b7e6fc250210ff123b1916f13da6ab2
NiGHTMARE <rsbgbn@126.com>
2019-08-20 14:50:27 +0800

    update weather_scrapy to v0.1.1

--- weather/weather/spiders/weather_spider.py
+++ weather/weather/spiders/weather_spider.py
@@ -1,7 +1,6 @@
 import scrapy
-# import re
+import re
 from weather.items import WeatherItem
-
 
 class WeatherSpider(scrapy.Spider):
     name = "weather_spider"
@@ -23,48 +22,38 @@ class WeatherSpider(scrapy.Spider):
         item = WeatherItem()
 
         # citys = response.xpath('//div[@class="conMidtab" and not(@style)]/div[@class="conMidtab3"]/table/tbody/tr')
-        citys = response.xpath('/html/body/div[4]/div[2]/div/div/div[2]/div[1]/div[2]/table/tbody/tr')
-
-        print("\n flag1 \n")
-
-        target = open("response.txt", 'wb+')
-        target.write(response.body)
-
-        target = open("citys.txt", 'w+')
-        for single in citys:
-            target.write(str(city) + "\n")
+        citys = response.xpath('//div[@class="conMidtab" and not (@style)]/div[@class="conMidtab3"]/table/tr')
 
         for city in citys:
-            print("\n flag2 \n")
-            item['city'] = city.xpath('.//td[@width=83]/a/text()').get()
-            print("city:" + item['city'])
-            item['weather_morning'] = city.xpath('.//td[@width=89]/text()').get()
-            item['weather_evening'] = city.xpath('.//td[@width=98]/text()').get()
+            item['city'] = city.xpath('./td[@width=83]/a/text()').get()
+            item['weather_morning'] = city.xpath('./td[@width=89]/text()').get()
+            item['weather_evening'] = city.xpath('./td[@width=98]/text()').get()
 
             # handle wind info
-            wind_morning_list = city.xpath('.//td[@width=162]').getall()
+            wind_morning_list = city.xpath('./td[@width=162]').getall()
             for wind_morning in wind_morning_list:
                 wind_morning_single = "".join(wind_morning.split())
 
                 # remove HTML tag
-                # wind_morning_single_matched = re.sub(r'<.*?>','',wind_morning_single)
+                wind_morning_single_matched = re.sub(r'<.*?>', '', wind_morning_single)
+                print(wind_morning_single_matched)
+                wind_morning_single_matched2 = re.sub(r'&lt;', '<', wind_morning_single_matched)
+                print(wind_morning_single_matched2)
 
-                # item['wind_morning'] = wind_morning_single_matched
-                item['wind_morning'] = wind_morning_single
+                item['wind_morning'] = wind_morning_single_matched2
 
-            wind_evening_list = city.xpath('.//td[@width=177]').getall()
+            wind_evening_list = city.xpath('./td[@width=177]').getall()
             for wind_evening in wind_evening_list:
                 wind_evening_single = "".join(wind_evening.split())
 
                 # remove HTML tag
-                # wind_evening_single_matched = re.sub(r'<.*?>','',wind_evening_single)
+                wind_evening_single_matched = re.sub(r'<.*?>', '', wind_evening_single)
+                wind_evening_single_matched2 = re.sub(r'&lt;', '<', wind_evening_single_matched)
 
-                # item['wind_evening'] = wind_evening_single_matched
-                item['wind_evening'] = wind_evening_single
+                item['wind_evening'] = wind_evening_single_matched2
 
             # get temperature from response
-            item['temperature_high'] = city.xpath('.//td[@width=92]/text()').get()
-            item['temperature_low'] = city.xpath('.//td[@width=86]/text()').get()
+            item['temperature_high'] = city.xpath('./td[@width=92]/text()').get()
+            item['temperature_low'] = city.xpath('./td[@width=86]/text()').get()
 
-            print("\n flag3 \n")
             yield item
