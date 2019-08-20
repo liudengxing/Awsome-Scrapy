c1534ec037baceb78b9b8587260b2aee09046714
NiGHTMARE <rsbgbn@126.com>
2019-08-20 17:21:59 +0800

    fix bugs

--- weather/weather/spiders/weather_spider.py
+++ weather/weather/spiders/weather_spider.py
@@ -11,6 +11,7 @@ class WeatherSpider(scrapy.Spider):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.3226.400 QQBrowser/9.6.11681.400'
     }
 
+
     def start_requests(self):
         start_urls = ["/textFC/beijing.shtml",\
         "/textFC/anhui.shtml",\
@@ -47,19 +48,15 @@ class WeatherSpider(scrapy.Spider):
         "/textFC/hongkong.shtml",\
         "/textFC/macao.shtml",\
         "/textFC/taiwan.shtml"]
-        
+
         for url in start_urls:
             yield scrapy.Request("http://www.weather.com.cn" + url)
 
 
     def parse(self, response):
 
-        global FLAG
-
-        # print(response.body)
         item = WeatherItem()
 
-        # citys = response.xpath('//div[@class="conMidtab" and not(@style)]/div[@class="conMidtab3"]/table/tbody/tr')
         citys = response.xpath('//div[@class="conMidtab" and not (@style)]/div[@class="conMidtab3"]/table/tr')
 
         for city in citys:
@@ -69,29 +66,27 @@ class WeatherSpider(scrapy.Spider):
 
             # handle wind info
             wind_morning_list = city.xpath('./td[@width=162]').getall()
-            for wind_morning in wind_morning_list:
-                wind_morning_single = "".join(wind_morning.split())
-
-                # remove HTML tag
-                wind_morning_single_matched = re.sub(r'<.*?>', '', wind_morning_single)
-                print(wind_morning_single_matched)
-                wind_morning_single_matched2 = re.sub(r'&lt;', '<', wind_morning_single_matched)
-                print(wind_morning_single_matched2)
-
-                item['wind_morning'] = wind_morning_single_matched2
+            item['wind_morning'] = self.stringSplit(wind_morning_list)
 
             wind_evening_list = city.xpath('./td[@width=177]').getall()
-            for wind_evening in wind_evening_list:
-                wind_evening_single = "".join(wind_evening.split())
-
-                # remove HTML tag
-                wind_evening_single_matched = re.sub(r'<.*?>', '', wind_evening_single)
-                wind_evening_single_matched2 = re.sub(r'&lt;', '<', wind_evening_single_matched)
-
-                item['wind_evening'] = wind_evening_single_matched2
+            item['wind_evening'] = self.stringSplit(wind_evening_list)
 
             # get temperature from response
             item['temperature_high'] = city.xpath('./td[@width=92]/text()').get()
             item['temperature_low'] = city.xpath('./td[@width=86]/text()').get()
 
             yield item
+
+
+    #
+    def stringSplit(self, content_list):
+        for content in content_list:
+            # delete all the blank
+            content_single = "".join(content.split())
+
+            # remove HTML tag
+            content_match = re.sub(r'<.*?>', '', content_single)
+            # replace "&lt;" with "<"
+            content_match2 = re.sub(r'&lt;', '<', content_match)
+
+            yield content_match2
